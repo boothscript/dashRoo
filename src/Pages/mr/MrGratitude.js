c7ac139d439c689c422b75a6129f4cbb37287479
Steve Booth <scriptmcuk@gmail.com>
2020-03-28 07:42:17 +0000

    replaced textareas on Mrgratitude

--- src/Pages/mr/MrGratitude.js
+++ src/Pages/mr/MrGratitude.js
@@ -1,14 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 import {
   MrContainer,
   MrHeader,
-  MrTextArea,
+  MrTextInput,
   MrFooter,
   MrMain
 } from "../../Components/mr";
 
 import useConfirmInputs from "../../Hooks/useConfirmInputs";
+
+const Div = styled.div`
+  grid-column: 1/-1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 function MrGratitude() {
   const { inputStates, confirmInput, checkInputs } = useConfirmInputs(3);
@@ -16,17 +24,17 @@ function MrGratitude() {
     <MrContainer>
       <MrHeader />
       <MrMain>
-        <MrTextArea
+        <MrTextInput
           inputId={0}
           confirmFn={confirmInput}
           placeholder={"Reason to be greatful #1"}
         />
-        <MrTextArea
+        <MrTextInput
           inputId={1}
           confirmFn={confirmInput}
           placeholder={"Reason to be greatful #2"}
         />
-        <MrTextArea
+        <MrTextInput
           inputId={2}
           confirmFn={confirmInput}
           placeholder={"Reason to be greatful #3"}
