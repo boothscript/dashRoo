5a5905f0d3ca68b48dc8e18b53d72999db503ff4
Steve Booth <scriptmcuk@gmail.com>
2020-05-14 11:01:24 +0100

    timer change

--- src/lib/Reducers/timerStackReducer.js
+++ src/lib/Reducers/timerStackReducer.js
@@ -11,7 +11,7 @@ import {
   UPDATE_EDIT_TIME,
 } from '../Actions/timerStackTypes';
 
-const durations = { session: 15000000, break: 300000, longBreak: 15000000 }; // time in seconds
+const durations = { session: 1500000, break: 300000, longBreak: 1500000 }; // time in seconds
 
 const initialState = {
   mode: 'session',
