31c3c7af0d23f98fa69bc34cf4caecf8d4850ddd
Steve Booth <scriptmcuk@gmail.com>
2020-06-17 14:00:33 +0100

    Add Link

---
+++ src/Atoms/Link/Link.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Link({ text, size = 'default', state, onButtonClick }) {
+  if (state === 'DISABLED') {
+    return (
+      <LinkA size={size} disabled>
+        {text}
+      </LinkA>
+    );
+  }
+  if (state === 'HIDDEN') {
+    return (
+      <LinkA size={size} hidden>
+        {text}
+      </LinkA>
+    );
+  }
+
+  return (
+    <LinkA size={size} onclick={onButtonClick}>
+      {text}
+    </LinkA>
+  );
+}
+
+const LinkA = styled.a`
+  color: ${(props) => props.theme.colors.white70};
+  font-family: ${(props) => props.theme.font.main};
+  font-weight: 400;
+  font-size: ${(props) => props.theme.font.button[props.size]};
+  visibility: ${(props) => (props.hidden ? 'hidden' : 'visible')};
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
+  text-decoration: none;
+  border-bottom: 1px solid ${(props) => props.theme.colors.white70};
+  &:hover {
+    color: ${(props) =>
+      props.disabled ? props.theme.colors.white70 : props.theme.colors.white90};
+    border-bottom: 1px solid
+      ${(props) =>
+        props.disabled
+          ? props.theme.colors.white70
+          : props.theme.colors.white90};
+  }
+  &:focus {
+    outline: solid 3px ${(props) => props.theme.colors.white90};
+  }
+`;
---
+++ src/Atoms/Link/Link.stories.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import Link from './Link';
+
+export default {
+  component: Link,
+  title: 'Link',
+  excludeStories: /.*Data$/,
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          padding: '3em',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
+};
+
+export const DefaultData = { text: 'Button', size: '', state: 'DEFAULT' };
+export const DisabledData = { ...DefaultData, state: 'DISABLED' };
+export const HiddenData = { ...DefaultData, state: 'HIDDEN' };
+
+export const Default = () => (
+  <>
+    <Link
+      text="Button"
+      state="DEFAULT"
+      size="large"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="DEFAULT"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="DEFAULT"
+      size="small"
+      onButtonClick={action('onButtonClick')}
+    />
+  </>
+);
+
+export const Disabled = () => (
+  <>
+    <Link
+      text="Button"
+      state="DISABLED"
+      size="large"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="DISABLED"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="DISABLED"
+      size="small"
+      onButtonClick={action('onButtonClick')}
+    />
+  </>
+);
+
+export const Hidden = () => (
+  <>
+    <Link
+      text="Button"
+      state="HIDDEN"
+      size="large"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="HIDDEN"
+      onButtonClick={action('onButtonClick')}
+    />
+    <Link
+      text="Button"
+      state="HIDDEN"
+      size="small"
+      onButtonClick={action('onButtonClick')}
+    />
+  </>
+);
