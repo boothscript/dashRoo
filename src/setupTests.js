89515e755d8bc26c03533f4f89f7e2261ef08f9b
Steve Booth <scriptmcuk@gmail.com>
2020-06-18 11:00:53 +0100

    Add jest styled components plugin

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,5 +6,6 @@ require('jest-localstorage-mock');
 import '@testing-library/jest-dom/extend-expect';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-styled-components';
 
 Enzyme.configure({ adapter: new Adapter() });
