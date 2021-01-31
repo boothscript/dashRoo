122bf853716ed0e992f7b5a8187acf73e23d6c40
Steve Booth <scriptmcuk@gmail.com>
2021-01-31 17:20:43 +0000

    added habit module

---
+++ src/Organisims/HabitModule/Habitmodule.stories.js
@@ -0,0 +1,41 @@
+// import React from 'react';
+// import { action } from '@storybook/addon-actions';
+
+// import HabitModule from './HabitModule';
+
+// export default {
+//   component: HabitModule,
+//   title: 'Habit Display',
+//   excludeStories: /.*Data$/,
+//   decorators: [
+//     (story) => (
+//       <div
+//         style={{
+//           padding: '3em',
+//           display: 'flex',
+//           justifyContent: 'space-between',
+//           alignItems: 'center',
+//         }}
+//       >
+//         {story()}
+//       </div>
+//     ),
+//   ],
+// };
+
+// export EmptyData = {}
+// export NormalCaseData = {}
+// export ManyHabitsCaseData = {}
+
+// const Empty = () =>{
+//     return <HabitModule />
+
+// }
+// const NormalCase = () => {
+//     return <HabitModule />
+
+// }
+// const ManyHabitsCase = () => {
+//     return <HabitModule />
+
+// }
